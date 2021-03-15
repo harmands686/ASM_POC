50e3af466d188cfb1168268a6ca743bf8f22e317
Harmandeep Singh <hsingh15@in.ibm.com>
2021-03-15 22:25:49 +0530

    serialization enabled

--- src/main/java/com/vf/model/ASMEdge.java
+++ src/main/java/com/vf/model/ASMEdge.java
@@ -1,5 +1,7 @@
 package com.vf.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -7,8 +9,13 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "asm_edge")
-public class ASMEdge {
+public class ASMEdge implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	private String app;
 	
--- src/main/java/com/vf/model/ASMResource.java
+++ src/main/java/com/vf/model/ASMResource.java
@@ -1,5 +1,7 @@
 package com.vf.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -7,8 +9,12 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "as_resource")
-public class ASMResource {
+public class ASMResource implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	private String app;
 	@Id
