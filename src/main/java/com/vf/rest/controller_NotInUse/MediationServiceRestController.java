9234c169b0755754f8d84500f0b1464c0cb3bfcb
Harmandeep Singh <hsingh15@in.ibm.com>
2021-03-16 16:50:01 +0530

    job fix

--- src/main/java/com/vf/rest/controller_NotInUse/MediationServiceRestController.java
+++ src/main/java/com/vf/rest/controller_NotInUse/MediationServiceRestController.java
@@ -50,15 +50,12 @@ public class MediationServiceRestController {
 			return false; // Job is already running
 	}
 	
-    @GetMapping("/startBulkJob/{id}/{provider}")
-    public String initiateJob(@PathVariable(value = "id") String JOBID,@PathVariable(value = "provider") String PROVIDER) throws ASMEdgeNotFoundException {
+    @GetMapping("/startBulkJob/{id}/{provider}/{sync}")
+    public String initiateJob(@PathVariable(value = "id") String JOBID,@PathVariable(value = "provider") String PROVIDER,@PathVariable(value = "sync") String SYNC) throws ASMEdgeNotFoundException {
 
+    	
+    	int statusCode2 = 0;
     	try{
-    		if(PROVIDER != null || !PROVIDER.equals("")){
-    		}
-    		else{
-    			PROVIDER = "Dummy_DataSource_Provider";
-    		}
 		    	List<ASMResource> asmResources = (ArrayList<ASMResource>) asmResourceRepo.findByApp("gb");
 		
 		    	List<ASMEdge> asmEdges = (ArrayList<ASMEdge>) asmEdgeRepo.findByApp("gb");
@@ -67,34 +64,39 @@ public class MediationServiceRestController {
 		    	int statusCode1 = asm_checkJobAlreadyRunning(JOBID);
 		    	System.out.println("StatusCode1="+statusCode1);
 		    	if(runJob(statusCode1) == true){
-		    		int statusCode2 = asm_startBulkJob(JOBID,PROVIDER);
+		    		statusCode2 = asm_startBulkJob(JOBID,PROVIDER);
 		    		System.out.println("statusCode2="+statusCode2);
 		    		System.out.println("Sleeping for 5 seconds");
 		    		Thread.sleep(5000);
-		    		if(statusCode2 != 201 ){// If not 201 then we failed
+		    	}
+		    	else{//Job already running
+		    		System.out.println("ASM "+JOBID+ " already running!!!");
+		    		//return "ASM "+JOBID+" already running!!!";
+		    		statusCode2 = 201; //Send the latest resources and edges again
+		    	}
+		    	if(statusCode2 != 201 ){// If not 201 then we failed
 		    			System.out.println("ASM "+JOBID+" failed to start!!!");
-		    		}else{// Result code is 201, ie. successful.
+		    	}else{// Result code is 201, ie. successful.
 		    			System.out.println("ASM "+JOBID+" started successfully");
 		    			asm_createResource(asmResources,JOBID);
 		    			//System.out.println("Sleeping for 5 seconds");
 		    			//Thread.sleep(5000);
 		    			asm_createEdge(asmEdges,JOBID);
-		    			int status3 = asm_syncBulkJob(JOBID);
-		    			System.out.println("statusCode3="+status3);
-		    			if (status3 != 200){ // If not 200 then we failed.
-		    				System.out.println("ASM "+JOBID+" Synchronized can not be completed.");
-		    				
-		    			  } else {  // Result code is 200, ie. successful.
-		    				  System.out.println("ASM "+JOBID+" Synchronized is completed successfully");
-		    				  
-		    			  }
-		    		}
-		    		
-		    	}else{//Job already running
-		    		System.out.println("ASM "+JOBID+ " already running!!!");
-		    		return "ASM "+JOBID+" already running!!!";
+		    			if(SYNC.equalsIgnoreCase("y"))
+		    			{
+		    				int status3 = asm_syncBulkJob(JOBID);
+			    			System.out.println("statusCode3="+status3);
+			    			if (status3 != 200){ // If not 200 then we failed.
+			    				System.out.println("ASM "+JOBID+" Synchronized can not be completed.");
+			    				
+			    			  } else {  // Result code is 200, ie. successful.
+			    				  System.out.println("ASM "+JOBID+" Synchronized is completed successfully");
+			    				  
+			    			  }
+		    			}
 		    	}
-		    	return "ASM " +JOBID+ " completed. To see status check logs!!!";
+		    		
+		    	return "ASM " +JOBID+ " completed. Sync="+ SYNC+" To see status check logs!!!";
     	
     	}catch (Exception e) {
 			// TODO: handle exception
